6d12caf205c6c373238574766912b4ce95be3a4f
Moaz <Moaz@example.com>
2024-10-09 10:31:08 +0300

    new

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SplashScreen());
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Splash Screen'),
+        ),
+      ),
+    );
+  }
+}
